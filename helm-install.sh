dc102c44c2033ebcc5731d3a6815caedd20d85f7
Jake Becker <jbecker@conviva.com>
2021-01-08 14:54:27 -0800

    first commit

--- helm-install.sh
+++ helm-install.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-APP="my-app"
-
-if ! kubectl get namespaces | grep "$APP"; then
-    kubectl create namespace $APP
-fi
-
 helm upgrade \
     --install \
-    --namespace $APP \
+    --namespace my-app \
+    --create-namespace \
     --set OAUTH2_PROXY_CLIENT_ID=$OAUTH2_PROXY_CLIENT_ID \
     --set OAUTH2_PROXY_CLIENT_SECRET=$OAUTH2_PROXY_CLIENT_SECRET \
     --set OAUTH2_PROXY_COOKIE_SECRET=$OAUTH2_PROXY_COOKIE_SECRET \
     --set url=my-app.example.com
     --set ingressSecretName=tls-secret
-    $APP \
+    my-app \
     .
